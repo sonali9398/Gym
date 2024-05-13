426ade4ffffd7878accd6bc8c2d3a53c4e59c809
sonali9398 <sonalikarangutkar9398@gmail.com>
2024-05-13 23:21:54 +0530

    search exercise

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -8,7 +8,7 @@ const HeroBanner = () => {
       <Typography  fontWeight='600' fontSize='26px'>
         Fitness Club
       </Typography>
-      <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'40px'}}}>
+      <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'40px'}}} mb='23px' mt='30px'>
         Sweat, Smile <br/> Repeat
       </Typography>
       <Typography fontSize='22px' lineHeight='35px' mb={3}>
@@ -19,7 +19,11 @@ const HeroBanner = () => {
         Explore Excercies
       </Button>
 
-      <img src={HeroImg} alt='hero'/>
+      <Typography fontWeight={600} color='FF2625' sx={{opacity:0.1, display:{lg:'block', xs:'none'}, fontsize:'200px'}} >
+        Excercise
+      </Typography>
+
+      <img src={HeroImg} alt='hero' className='hero-banner-img'/>
     </Box>
   )
 }
